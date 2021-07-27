55433552556e644cb0924ce0e0d59079b689ea53
Matthew <m_kmorgan@hotmail.com>
2021-07-26 21:18:23 -0600

    created item boxes to fill the food menu

--- pages/Food/Food.js
+++ pages/Food/Food.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, ScrollView} from "react-native";
+import Item from "./Item";
 
 export default function Food({ navigation }) {
   return (
     <View>
       <Text>FOOD</Text>
+      <ScrollView>
+        <View style={styles.foodContainer}>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+          <Item/>
+        </View>
+
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  foodContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: 'space-evenly',
+
+  }
+});
---
+++ pages/Food/Item.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function Item() {
+  return (
+    <View style={styles.container}>
+      <Text>Item</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 180,
+    height: 180,
+    backgroundColor: "skyblue",
+    borderRadius: 10,
+    marginVertical: 12
+
+  }
+})
